f714422c2e06066b07d172f0d998ad8361d638bb
loic <loic.senecat@bsd.fr>
2022-05-18 11:24:01 +0200

    add user creation

---
+++ client/src/providers/models/Quiz.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type Quiz = {
+    name: string;
+    date_creation: string;
+    id: string;
+    user_id: number;
+};
--- server/api/main.py
+++ server/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from src.routes import users, quiz
 from src.database import database
+from fastapi.middleware.cors import CORSMiddleware
 import src.models
 
 src.models.Base.metadata.create_all(bind=database.engine)
@@ -8,3 +9,10 @@ src.models.Base.metadata.create_all(bind=database.engine)
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(quiz.router)
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=['*'],
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*'],
+)
